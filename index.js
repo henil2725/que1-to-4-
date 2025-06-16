5dce8cddbe86a216d0b27b12fcaa6b9af4f6df5b
henil2725 <senjaliyahenil001@gmail.com>
2025-06-16 11:08:16 +0530

    que 1 to 4

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("name","henil senjaliya" )
+console.log("age","20")
+
+
+
+let title = "henil";
+console.log(title);
+
+ title ="jitendra bhai";
+ console.log("name",title);
+
+ title ="daksha ben";
+ console.log("name",title);
+
+ let  myName="henil"
+ let age=20;
+ let sentence = "my name is "+ myName +" and my age " + age + " ! ";
+ console.log(sentence);
+
+
+
+ let studentName = "henil";
+let school = "LJ Univercity";
+let grade = "4th sem";
+let section = "E";
+let rollNo = 23;
+let marks1 = 85;
+let marks2 = 90;
+let marks3 = 78;
+
+console.log("=================================");
+console.log("         STUDENT REPORT CARD     ");
+console.log("=================================");
+console.log("Name     : " + studentName);
+console.log("School   : " + school);
+console.log("Grade    : " + grade);
+console.log("Section  : " + section);
+console.log("Roll No  : " + rollNo);
+console.log("---------------------------------");
+console.log("Subject 1: " + marks1);
+console.log("Subject 2: " + marks2);
+console.log("Subject 3: " + marks3);
+console.log("=================================");
