7501188a0642c2e30d863505b88aa75d474f881d
henil2725 <senjaliyahenil001@gmail.com>
2025-06-16 11:11:48 +0530

    ifelse

--- index.js
+++ index.js
@@ -1,43 +1,141 @@
-console.log("name","henil senjaliya" )
-console.log("age","20")
+ 
+ //q1
+ pi =3.14;
+ radius = 7;
+let area = 3.14 *radius**2;
+console.log("the area of circle is :" + area);
 
 
+//q2
 
-let title = "henil";
-console.log(title);
+let a = 10;
+let b = 20;
+let c = 5;
 
- title ="jitendra bhai";
- console.log("name",title);
+console.log  ((a * b) / c + (b - a));
+ 
+//q3
 
- title ="daksha ben";
- console.log("name",title);
+let x = 5;
+let y = 10;
 
- let  myName="henil"
- let age=20;
- let sentence = "my name is "+ myName +" and my age " + age + " ! ";
- console.log(sentence);
+ x += 2;
+ y *= x;
+ console.log(" letest value of x ",x);
+ console.log(" letest value of y ",y);
 
 
+//q4
 
- let studentName = "henil";
-let school = "LJ Univercity";
-let grade = "4th sem";
-let section = "E";
-let rollNo = 23;
-let marks1 = 85;
-let marks2 = 90;
-let marks3 = 78;
+let num = 2;
 
-console.log("=================================");
-console.log("         STUDENT REPORT CARD     ");
-console.log("=================================");
-console.log("Name     : " + studentName);
-console.log("School   : " + school);
-console.log("Grade    : " + grade);
-console.log("Section  : " + section);
-console.log("Roll No  : " + rollNo);
-console.log("---------------------------------");
-console.log("Subject 1: " + marks1);
-console.log("Subject 2: " + marks2);
-console.log("Subject 3: " + marks3);
-console.log("=================================");
+num += 15;
+num -= 5;
+num *= 3;
+num /= 2;
+console.log("answer",num);
+
+//q5
+
+let score1 = 85;
+let score2 = 90;
+let result1 = score1 > 80 && score2 > 80;
+console.log("score1 and score2 are greater than 80 :",result1);
+
+//q6
+
+let fruit1 = "apple";
+let fruit2 = "banana";
+
+let fruit = fruit1 < fruit2;
+console.log(" apple less than banana : ",fruit);
+
+
+//q7
+
+let age = 25;
+let hasDrivingLicense = true;
+if(age >= 18 && hasDrivingLicense){
+    console.log("eligible for drive");
+}else{
+    console.log("not eligible for drive");
+}
+
+//q8
+//query
+
+
+//q9
+let string = 123.45;
+let result2 = Number(string) + 10;
+console.log(result2);
+
+
+//q10
+
+let booleanValue = true ;
+let result3 = booleanValue + " is this value";
+console.log(result3);
+
+//q11
+
+
+//q13
+
+let number = 9;
+if(number > 10){
+    console.log("this is postive number");
+}
+else if(number >8){
+    console.log("this is nagative number");
+}
+
+//q14
+
+let temperature = 30;
+if (temperature > 25){
+    console.log("it's the hot day");
+}
+else if(temperature > 30){
+    console.log("it's the cool day")
+}
+
+//q15
+let num1 = 8 ;
+
+if(num1 > 0){
+    console.log("positive");
+}
+else if(num1 < 8){
+    console.log("negative");
+}
+
+//q16
+let marks = 75;
+if(marks >= 90){
+    console.log("A grade");
+}
+else if(marks >= 80 && marks < 89){
+    console.log("B grade");
+}
+else if(marks >= 70 && marks < 79){
+    console.log("C grade");
+}
+else if(marks >= 60 && marks < 69){
+    console.log("D grade");
+}
+
+
+//q17
+let number1 = 15;
+let number2 = 20;
+if(number1 > number2){
+    console.log("number1 is greater than number2");
+}
+else if(number1 < number2){
+    console.log("number1 is less than number2");
+}
+else{
+    console.log("number1 is equal to number2");
+}
+//q18
